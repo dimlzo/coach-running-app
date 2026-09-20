49b25b6705e346c6a386db204fde7b67b7086ff7
dimlzo <dimitriloizeau@gmail.com>
2026-09-20 17:48:44 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CoachRunning from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<CoachRunning />);
